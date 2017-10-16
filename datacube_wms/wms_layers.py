4c5506f6ed0ce527834fbf9bea655ede6c8dbdc9
Paul Haesler <paul.haesler@nicta.com.au>
2017-10-16 11:53:32 +1100

    First pass time and space bounds. (Not scalable)

--- datacube_wms/wms_layers.py
+++ datacube_wms/wms_layers.py
@@ -1,5 +1,21 @@
-from .wms_cfg import layer_cfg
+from .wms_cfg import service_cfg, layer_cfg
 import datacube
+
+def accum_min(a, b):
+    if a is None:
+        return b
+    elif b is None:
+        return a
+    else:
+        return min(a,b)
+
+def accum_max(a, b):
+    if a is None:
+        return b
+    elif b is None:
+        return a
+    else:
+        return max(a,b)
 
 class ProductLayerDef(object):
     def __init__(self, product_cfg, platform_def, dc=None):
@@ -10,12 +26,58 @@ class ProductLayerDef(object):
         self.product_variant = product_cfg["variant"]
         if not dc:
             dc = datacube.Datacube(app="wms")
+        self.dc = dc
         self.product = dc.index.products.get_by_name(self.name)
         self.definition = self.product.definition
         self.title = "%s %s %s (%s)" % (platform_def.title, 
                 self.product_variant,
                 self.product_type,
                 self.product_label)
+        self._ranges = None
+
+    @property
+    def ranges(self):
+        # TODO: This will not scale.
+        if self._ranges is None:
+            self._ranges = self._determine_ranges()
+        return self._ranges
+
+    def _determine_ranges(self):
+        r = {
+            "lat": {
+                "min": None,
+                "max": None
+            },
+            "lon": {
+                "min": None,
+                "max": None
+            },
+            "time_set": set(),
+            "extents": { crs: None for crs in service_cfg["published_CRSs"] }
+        }
+        crses = { crs: datacube.utils.geometry.CRS(crs) for crs in service_cfg["published_CRSs"] }
+
+        for ds in self.dc.find_datasets(product=self.name):
+            r["lat"]["min"] = accum_min(r["lat"]["min"], ds.metadata.lat.begin)
+            r["lat"]["max"] = accum_max(r["lat"]["max"], ds.metadata.lat.end)
+            r["lon"]["min"] = accum_min(r["lon"]["min"], ds.metadata.lon.begin)
+            r["lon"]["max"] = accum_max(r["lon"]["max"], ds.metadata.lon.end)
+
+            r["time_set"].add(ds.center_time.date())
+            
+            for crsid in service_cfg["published_CRSs"]:
+                crs = crses[crsid]
+                ext = ds.extent
+                if ext.crs != crs:
+                    ext = ext.to_crs(crs)
+                if r["extents"][crsid] is None:
+                    r["extents"][crsid] = ext
+                else:
+                    r["extents"][crsid] = r["extents"][crsid].union(ext)
+                
+        r["times"] = sorted(r["time_set"])
+        r["bboxes"] = { crsid: r["extents"][crsid].boundingbox for crsid in service_cfg["published_CRSs"] }
+        return r
 
 class PlatformLayerDef(object):
     def __init__(self, platform_cfg, prod_idx, dc=None):
