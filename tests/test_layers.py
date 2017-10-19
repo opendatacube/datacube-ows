54eb8bad22b2b84ec576c6f2cbeece0bd62fec16
Paul Haesler <paul.haesler@nicta.com.au>
2017-10-20 09:14:04 +1100

    Fixed broken test.

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -3,12 +3,8 @@ from datacube_wms.wms_layers import get_layers
 import datacube
 import pytest
 
-@pytest.fixture
-def wms_datacube(request):
-    return datacube.Datacube(app="wms_test")
-
-def test_get_layers(wms_datacube):
-    layers = get_layers(dc=wms_datacube)
+def test_get_layers():
+    layers = get_layers()
 
     assert layers.platforms
     for p in layers:
