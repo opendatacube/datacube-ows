7a6b5dc8812e12edf59ec0eb167ade5f05b73075
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-06-28 16:00:53 +1000

    Support products that have no native CRS

--- datacube_wms/ogc_utils.py
+++ datacube_wms/ogc_utils.py
@@ -1,4 +1,7 @@
-from datacube_wms.wms_cfg_local import response_cfg
+try:
+    from datacube_wms.wms_cfg_local import service_cfg, layer_cfg, response_cfg
+except:
+    from datacube_wms.wms_cfg import service_cfg, layer_cfg, response_cfg
 
 
 def resp_headers(d):
