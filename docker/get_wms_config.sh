40132424949aec0af3d680661358b96cf2a32b3d
Robert Armstrong <robert.armstrong@ga.gov.au>
2018-11-05 10:19:46 +1100

    Allow config to come from a file system path in docker image

--- docker/get_wms_config.sh
+++ docker/get_wms_config.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-if [ "$WMS_CONFIG_URL" ]
+if [ -e "$WMS_CONFIG_PATH" ]
+then
+    cp "$WMS_CONFIG_PATH" /code/datacube_wms/wms_cfg.py
+elif [ "$WMS_CONFIG_URL" ]
 then
     A=$$; ( wget -q "$WMS_CONFIG_URL" -O $A.d && mv $A.d /code/datacube_wms/wms_cfg.py ) || (rm $A.d; echo "Failed to download WMS config file")
 fi
