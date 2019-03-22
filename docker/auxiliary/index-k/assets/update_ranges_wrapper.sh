967a1dda21233195e2c878fd3ea742de2b371aeb
tom-butler <tombutler.development@gmail.com>
2019-03-22 15:27:57 +1100

    fix missing '$' on DC IGNORE LINEAGE

--- docker/auxiliary/index-k/assets/update_ranges_wrapper.sh
+++ docker/auxiliary/index-k/assets/update_ranges_wrapper.sh
@@ -2,4 +2,4 @@
 # Wraps the update_ranges.sh script by passing in environment variables
 # from the Docker's environment variables
 
-indexing/update_ranges.sh -b "$DC_S3_INDEX_BUCKET" -p "$DC_S3_INDEX_PREFIX" -s "$DC_S3_INDEX_SUFFIX" -y "$DC_INDEX_YAML_SAFETY" -d "$DC_RANGES_PRODUCT" -l "DC_IGNORE_LINEAGE"
+indexing/update_ranges.sh -b "$DC_S3_INDEX_BUCKET" -p "$DC_S3_INDEX_PREFIX" -s "$DC_S3_INDEX_SUFFIX" -y "$DC_INDEX_YAML_SAFETY" -d "$DC_RANGES_PRODUCT" -l "$DC_IGNORE_LINEAGE"
