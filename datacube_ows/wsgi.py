63838169dbcc49d242e96af07c79ba419306bbfe
Pin Jin <pin.jin@ga.gov.au>
2019-12-17 05:54:37 +0000

    Fix path typo

--- datacube_ows/wsgi.py
+++ datacube_ows/wsgi.py
@@ -3,7 +3,7 @@ import sys
 import os
 
 # This is the directory of the source code that the web app will run from
-sys.path.append("/opt/config")
+sys.path.append("/opt")
 
 # The location of the datcube config file.
 os.environ.setdefault("DATACUBE_CONFIG_PATH", "/opt/odc/.datacube.conf.local")
