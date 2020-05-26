3775764bb98f5bdbd83a66acacaa5ffa4a1cc7ed
phaesler <paul.haesler@data61.csiro.au>
2020-05-26 10:41:26 +1000

    WCS2 github check.

--- ows_test_cfg.py
+++ ows_test_cfg.py
@@ -1077,7 +1077,10 @@ ows_cfg = {
                 # Renderer is the FQN of a Python function that takes:
                 #   * A WCSRequest object
                 #   * Some ODC data to be rendered.
-                "renderer": "datacube_ows.wcs_utils.get_tiff",
+                "renderers": {
+                    "1": "datacube_ows.wcs1_utils.get_tiff",
+                    "2": "datacube_ows.wcs2_utils.get_tiff",
+                },
                 # The MIME type of the image, as used in the Http Response.
                 "mime": "image/geotiff",
                 # The file extension to add to the filename.
@@ -1086,7 +1089,10 @@ ows_cfg = {
                 "multi-time": False
             },
             "netCDF": {
-                "renderer": "datacube_ows.wcs_utils.get_netcdf",
+                "renderers": {
+                    "1": "datacube_ows.wcs1_utils.get_netcdf",
+                    "2": "datacube_ows.wcs2_utils.get_netcdf",
+                },
                 "mime": "application/x-netcdf",
                 "extension": "nc",
                 "multi-time": True,
