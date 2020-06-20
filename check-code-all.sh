686eec5d3d1af58a63ee1927a3fc3e26e8c00b15
Tisham Dhar <whatnickd@gmail.com>
2020-06-20 21:23:14 +1000

    #287 Enable integration tests
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

---
+++ check-code-all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Convenience script for running Travis-like checks.
+
+set -eu
+set -x
+
+pylint -j 2 --reports no datacube_ows --disable=C,R
+
+# Run tests, taking coverage.
+# Users can specify extra folders as arguments.
+python3 -m pytest --cov=datacube_ows --cov-report=xml tests/
+cp coverage.xml /mnt/artifacts
+
+set +x
