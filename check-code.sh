3f0f34c771508a54eb3d875cf98b26258a2e2e50
Tisham Dhar <whatnickd@gmail.com>
2020-06-21 16:01:05 +1000

    Typo
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

--- check-code.sh
+++ check-code.sh
@@ -8,7 +8,7 @@ pylint -j 2 --reports no datacube_ows --disable=C,R
 
 # Run tests, taking coverage.
 # Users can specify extra folders as arguments.
-python3 -m pytest --cov=datacube_ows --cov-report=xml tests/ --ignore tests/test_wms_server.py --ignore tests/test_layer.py
+python3 -m pytest --cov=datacube_ows --cov-report=xml tests/ --ignore tests/test_wms_server.py --ignore tests/test_layers.py
 cp coverage.xml /mnt/artifacts
 
 set +x
