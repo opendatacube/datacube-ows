84ca88872db91050b95544246be9f7e34ffaab3e
Tisham Dhar <tisham.dhar@ga.gov.au>
2020-06-22 10:25:02 +1000

    #287 Quick hack to improve coverage (#291)
    * #287 Quick hack to improve coverage
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * #287 Cover legends endpoint
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * #287 consolidate fixture into conftest
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

---
+++ tests/test_no_db_routes.py
@@ -0,0 +1,38 @@
+"""Run with no DB to simulate connection failure
+"""
+
+def test_db_connect_fail(flask_client):
+    """Start with a database connection"""
+
+    rv = flask_client.get('/ping')
+    assert rv.status_code == 500
+
+def test_wcs_fail(flask_client):
+    """WCS endpoint fails"""
+
+    rv = flask_client.get('/wcs')
+    assert rv.status_code == 500
+
+def test_wms_fail(flask_client):
+    """WMS endpoint fails"""
+
+    rv = flask_client.get('/wms')
+    assert rv.status_code == 500
+
+def test_wmts_fail(flask_client):
+    """WMTS endpoint fails"""
+
+    rv = flask_client.get('/wmts')
+    assert rv.status_code == 500
+
+def test_legend_fail(flask_client):
+    """Fail on legend"""
+
+    rv = flask_client.get("/legend/layer/style/legend.png")
+    assert rv.status_code == 500
+
+def test_index_fail(flask_client):
+    """Base index endpoint fails"""
+
+    rv = flask_client.get('/')
+    assert rv.status_code == 500
