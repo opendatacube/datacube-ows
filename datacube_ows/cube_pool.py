92a9f6b07e3779a7c10878a7dbfb6f542bb6841f
phaesler <paul.haesler@data61.csiro.au>
2020-06-22 13:09:20 +1000

    Lint tweak

--- datacube_ows/cube_pool.py
+++ datacube_ows/cube_pool.py
@@ -30,6 +30,7 @@ class CubePool():
         try:
             c = self._new_cube()
             self._cubes[c] = True
+        # pylint: disable:bare-except
         except:
             c = None
         finally:
