468eea34421941e8ad4569a1f9bf46de33d0b194
Tisham Dhar <tisham.dhar@ga.gov.au>
2020-06-23 11:27:06 +1000

    #287 Make integration tests possible in CI (#292)
    * #287 Make integration tests possible in CI
    
    * Had exec command wrong
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * WSL vs CI compose image name
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * New path to checkcode
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * #287 Enable integration tests
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Make check code all executable
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Add owslib for integration tests
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Run without XML reporting
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Map postgres container correctly
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * #287 Use compose better
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Better docker compose with abs paths
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Better exec and coverage threshold
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Don't exit on failed tests and push coverage
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Treat unit test coverage separately
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Test rolling back coverage to address sqlite bug
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Try old coverage version
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Try to run tests as root
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Run tests as ows
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Send coverage temp file to /tmp
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Stash reports in /tmp
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Let ows own artifacts
    
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>
    
    * Make artifacts global read-write
    
    * #287 Include unit test coverage separately
    
    * Try to upload to codecovs at once
    
    * Comma separator for multiple files to Codecov
    
    * Go back to single file upload at a time

--- check-code-all.sh
+++ check-code-all.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 # Convenience script for running Travis-like checks.
-
-set -eu
 set -x
-
-pylint -j 2 --reports no datacube_ows --disable=C,R
 
 # Run tests, taking coverage.
 # Users can specify extra folders as arguments.
-python3 -m pytest --cov=datacube_ows --cov-report=xml tests/ --ignore tests/test_wms_server.py --ignore tests/test_layers.py
+python3 -m pytest --cov=datacube_ows --cov-report=xml tests/
 cp /tmp/coverage.xml /mnt/artifacts
 
 set +x
