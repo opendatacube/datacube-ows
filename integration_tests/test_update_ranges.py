1b2920ba7ef140553285eccd14a611117d8d4753
Tisham Dhar <whatnickd@gmail.com>
2020-06-24 18:02:03 +1000

    More update ranges tests
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

--- integration_tests/test_update_ranges.py
+++ integration_tests/test_update_ranges.py
@@ -1,10 +1,20 @@
 """Test update ranges on DB using Click testing
 https://click.palletsprojects.com/en/7.x/testing/
 """
+import os
+
 from datacube_ows.update_ranges import main
+
+def test_updates_ranges_schema(runner):
+    result = runner.invoke(main,["--schema","--role",os.getenv("DB_USERNAME")])
+    assert result.exit_code == 0
 
 def test_update_ranges_views(runner):
     result = runner.invoke(main,["--views","--blocking"])
+    assert result.exit_code == 0
+
+def test_update_version(runner):
+    result = runner.invoke(main,["--version"])
     assert result.exit_code == 0
 
 def test_update_ranges(runner):
