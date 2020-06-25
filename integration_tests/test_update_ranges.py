8bdd7791510b8aa5cc71038d4adae6a3a44bfc3c
Tisham Dhar <tisham.dhar@ga.gov.au>
2020-06-25 02:26:23 +0000

    #287 Add more tests

--- integration_tests/test_update_ranges.py
+++ integration_tests/test_update_ranges.py
@@ -13,8 +13,15 @@ def test_update_ranges_views(runner):
     result = runner.invoke(main,["--views","--blocking"])
     assert result.exit_code == 0
 
+    result = runner.invoke(main,["--views"])
+    assert result.exit_code == 0
+
 def test_update_version(runner):
     result = runner.invoke(main,["--version"])
+    assert result.exit_code == 0
+
+def test_update_ranges_product(runner):
+    result = runner.invoke(main,["ls8_usgs_level1_scene"])
     assert result.exit_code == 0
 
 def test_update_ranges(runner):
