d1c6b92a52104b76580dc8c09ef43fac445fbb07
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-06-25 07:13:59 +0000

    Move wms_server fixture to ows_server shared connection

--- integration_tests/conftest.py
+++ integration_tests/conftest.py
@@ -1,4 +1,8 @@
+import os
 import pytest
+
+from datacube_ows import ogc
+from pytest_localserver.http import WSGIServer
 
 from datacube_ows.ogc import app
 
@@ -6,3 +10,23 @@ from datacube_ows.ogc import app
 def flask_client():
     with app.test_client() as client:
         yield client
+
+
+class generic_obj(object):
+    pass
+
+@pytest.fixture
+def ows_server(request):
+    """
+    Run the WMS server for the duration of these tests
+    """
+    external_url = os.environ.get("SERVER_URL")
+    if external_url:
+        server = generic_obj()
+        server.url = external_url
+    else:
+        server = WSGIServer(application=ogc.app)
+        server.start()
+        request.addfinalizer(server.stop)
+
+    return server
--- integration_tests/test_wmts_server.py
+++ integration_tests/test_wmts_server.py
@@ -1,13 +1,12 @@
 import pytest
-
-from owslib.wcs import WebCoverageService
+from owslib.wmts import WebMapTileService
 from urllib import request
 from lxml import etree
 from imghdr import what
 
 
 def get_xsd(name):
-    xsd_f = request.urlopen("http://schemas.opengis.net/wcs/1.0.0/" + name)
+    xsd_f = request.urlopen("http://schemas.opengis.net/wms/1.3.0/" + name)
     schema_doc = etree.parse(xsd_f)
 
     return etree.XMLSchema(schema_doc)
@@ -27,7 +26,7 @@ def check_wcs_error(url, expected_error_message=None, expected_status_code=400):
         resp_xml = etree.XML(resp_content)
 
         # Validate response against Schema
-        ex_xsd = get_xsd("OGC-exception.xsd")
+        ex_xsd = get_xsd("exceptions_1_3_0.xsd")
         assert ex_xsd.validate(resp_xml)
 
         # Confirm error message is appropriate, ignore case
@@ -37,36 +36,39 @@ def check_wcs_error(url, expected_error_message=None, expected_status_code=400):
 
 def test_no_request(ows_server):
     # Make empty request to server:
-    check_wcs_error(ows_server.url + "/wcs", "No operation specified", 400)
+    check_wcs_error(ows_server.url + "/wmts", "No operation specified", 400)
 
 
 def test_invalid_operation(ows_server):
     # Make invalid operation request to server:
-    check_wcs_error(ows_server.url + "/wcs?request=NoSuchOperation", "Unrecognised operation: NoSuchOperation", 400)
+    check_wcs_error(ows_server.url + "/wmts?request=NoSuchOperation", "Unrecognised operation: NoSuchOperation", 400)
 
 
 def test_getcap_badsvc(ows_server):
     # Make bad service request to server:
-    check_wcs_error(ows_server.url + "/wcs?request=GetCapabilities&service=NotWCS", "Invalid service", 400)
+    check_wcs_error(ows_server.url + "/wmts?request=GetCapabilities&service=NotWMTS", "Invalid service", 400)
 
 
 @pytest.mark.xfail(reason="OWS Getcaps don't pass XSD")
 def test_getcap(ows_server):
-    resp = request.urlopen(ows_server.url + "/wcs?request=GetCapabilities&service=WCS&version=1.3.0", timeout=10)
+    resp = request.urlopen(ows_server.url + "/wmts?request=GetCapabilities&service=WCS&version=1.3.0", timeout=10)
 
     # Confirm success
     assert resp.code == 200
 
     # Validate XML Schema
     resp_xml = etree.parse(resp.fp)
-    gc_xds = get_xsd("wcsCapabilities.xsd")
+    gc_xds = get_xsd("wmtsGetCapabilities_response.xsd")
     assert gc_xds.validate(resp_xml)
 
 
-def test_wcs1_server(ows_server):
+def test_wmts_server(ows_server):
     # Use owslib to confirm that we have a somewhat compliant WCS service
-    wcs = WebCoverageService(url=ows_server.url+"/wcs", version="2.0.1")
+    wmts = WebMapTileService(url=ows_server.url+"/wmts")
+
+    assert wmts.identification.type == "OGS WMTS"
+    assert wmts.identification.version == "1.0.0"
 
     # Ensure that we have at least some layers available
-    contents = list(wcs.contents)
+    contents = list(wmts.contents)
     assert contents
