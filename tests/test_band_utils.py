538f2cace326bea2481b775eae5ba3d901556cfc
Tisham Dhar <whatnickd@gmail.com>
2020-07-12 22:43:11 +1000

    #287 Use dummy product to test band name aliasing
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

--- tests/test_band_utils.py
+++ tests/test_band_utils.py
@@ -16,9 +16,12 @@ from datacube_ows.band_utils import (
     sentinel2_ndci,
     multi_date_delta,
 )
+from datacube_ows.ows_configuration import BandIndex, OWSProductLayer
 
 
 class MockArray(xr.DataArray):
+    __slots__ = ("nodata",)
+
     def __init__(
         self,
         data,
@@ -40,6 +43,15 @@ TEST_XARR = {"b1": TEST_ARR_1, "b2": TEST_ARR_2}
 TEST_XARR_T = xr.Dataset({"b1": (["x", "y", "time"], np.ones((100, 100, 2)))})
 
 
+@pytest.fixture
+def dummy_layer():
+    product_layer = OWSProductLayer.__new__(OWSProductLayer)
+    product_layer.name = "test_product"
+    product_layer.band_idx = BandIndex.__new__(BandIndex)
+    product_layer.band_idx._idx = {"b1": "b1", "b2": "b2"}
+    return product_layer
+
+
 def test_scale_data():
     assert not scale_data(TEST_ARR_1, [0.0, 1.0], [0.0, 1.0]) is None
 
@@ -48,24 +60,28 @@ def test_sum_bands():
     assert not sum_bands(TEST_XARR, "b1", "b2") is None
 
 
-def test_norm_diff():
+def test_norm_diff(dummy_layer):
     assert not norm_diff(TEST_XARR, "b1", "b2") is None
+    assert not norm_diff(TEST_XARR, "b1", "b2", dummy_layer, scale_from=[0, 1]) is None
 
 
-def test_constant():
+def test_constant(dummy_layer):
     assert not constant(TEST_XARR, "b1", 10) is None
+    assert not constant(TEST_XARR, "b1", 10, dummy_layer) is None
 
 
-def test_band_quotient():
+def test_band_quotient(dummy_layer):
     assert not band_quotient(TEST_XARR, "b1", "b2") is None
+    assert not band_quotient(TEST_XARR, "b1", "b2", dummy_layer) is None
 
 
 def test_band_quotient_sum():
     assert not band_quotient_sum(TEST_XARR, "b1", "b2", "b1", "b2") is None
 
 
-def test_single_band_log():
+def test_single_band_log(dummy_layer):
     assert not single_band_log(TEST_XARR, "b1", 1.0, 1.0) is None
+    assert not single_band_log(TEST_XARR, "b1", 1.0, 1.0, dummy_layer) is None
 
 
 def test_single_band():
