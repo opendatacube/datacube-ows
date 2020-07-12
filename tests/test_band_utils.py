4cc95a6ac88fc91c44315aef822afad18100095b
Tisham Dhar <whatnickd@gmail.com>
2020-07-12 22:23:10 +1000

    #287 Mock Datasets with Dict
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

--- tests/test_band_utils.py
+++ tests/test_band_utils.py
@@ -5,57 +5,76 @@ import numpy as np
 import xarray as xr
 
 from datacube_ows.band_utils import (
-    scale_data, sum_bands, norm_diff,
-    constant, single_band, band_quotient,
-    band_quotient_sum, single_band_log,
-    sentinel2_ndci, multi_date_delta
+    scale_data,
+    sum_bands,
+    norm_diff,
+    constant,
+    single_band,
+    band_quotient,
+    band_quotient_sum,
+    single_band_log,
+    sentinel2_ndci,
+    multi_date_delta,
 )
 
-TEST_ARR_1 = xr.DataArray(np.ones((100, 100), dtype=np.uint32), attrs={'nodata':0})
-TEST_ARR_1.__setitem__('nodata', 0)
-TEST_ARR_2 = xr.DataArray(np.ones((100, 100), dtype=np.uint32), attrs={'nodata':0})
-TEST_ARR_2.__setitem__('nodata', 0)
-TEST_XARR = xr.Dataset(
-    {
-        'b1' : (['x', 'y'], TEST_ARR_1),
-        'b2' : (['x', 'y'], TEST_ARR_2),
-    }, attrs={'nodata':0}
-)
 
-TEST_XARR_T = xr.Dataset(
-    {
-        'b1' : (['x', 'y', 'time'], np.ones((100, 100, 2)))
-    }
-)
+class MockArray(xr.DataArray):
+    def __init__(
+        self,
+        data,
+        coords=None,
+        dims=None,
+        name=None,
+        attrs=None,
+        indexes=None,
+        fastpath=None,
+    ):
+        super().__init__(data, coords, dims, name, attrs, indexes, fastpath)
+        self.nodata = 0
+
+
+TEST_ARR_1 = MockArray(np.ones((100, 100), dtype=np.uint32), attrs={"nodata": 0})
+TEST_ARR_2 = MockArray(np.ones((100, 100), dtype=np.uint32), attrs={"nodata": 0})
+TEST_XARR = {"b1": TEST_ARR_1, "b2": TEST_ARR_2}
+
+TEST_XARR_T = xr.Dataset({"b1": (["x", "y", "time"], np.ones((100, 100, 2)))})
+
 
 def test_scale_data():
-    assert not scale_data(TEST_ARR_1, [0.0, 1.0] , [0.0, 1.0]) is None
+    assert not scale_data(TEST_ARR_1, [0.0, 1.0], [0.0, 1.0]) is None
+
 
 def test_sum_bands():
-    assert not sum_bands(TEST_XARR, 'b1', 'b2') is None
+    assert not sum_bands(TEST_XARR, "b1", "b2") is None
 
-@pytest.mark.xfail(reason="Nodata attributes need to be passed")
+
 def test_norm_diff():
-    assert not norm_diff(TEST_XARR, 'b1', 'b2') is None
+    assert not norm_diff(TEST_XARR, "b1", "b2") is None
+
 
 def test_constant():
-    assert not constant(TEST_XARR, 'b1', 10) is None
+    assert not constant(TEST_XARR, "b1", 10) is None
+
 
 def test_band_quotient():
-    assert not band_quotient(TEST_XARR, 'b1', 'b2') is None
+    assert not band_quotient(TEST_XARR, "b1", "b2") is None
+
 
 def test_band_quotient_sum():
-    assert not band_quotient_sum(TEST_XARR, 'b1', 'b2', 'b1', 'b2') is None
+    assert not band_quotient_sum(TEST_XARR, "b1", "b2", "b1", "b2") is None
+
 
 def test_single_band_log():
-    assert not single_band_log(TEST_XARR, 'b1', 1.0, 1.0) is None
+    assert not single_band_log(TEST_XARR, "b1", 1.0, 1.0) is None
+
 
 def test_single_band():
-    assert not single_band(TEST_XARR, 'b1') is None
+    assert not single_band(TEST_XARR, "b1") is None
+
 
 def test_multidate():
     assert not multi_date_delta(TEST_XARR_T) is None
 
-@pytest.mark.xfail(reason="Nodata attributes need to be passed")
+
 def test_ndci():
-    assert not sentinel2_ndci(TEST_XARR, 'b1', 'b2', 'b1', 'b2') is None
+    assert not sentinel2_ndci(TEST_XARR, "b1", "b2", "b1", "b2") is None
