b4ba81d63cb366ed35235bfe9d691795787a9aeb
phaesler <paul.haesler@data61.csiro.au>
2020-07-24 11:45:16 +1000

    No db route tests use provided minimal config.

--- tests/test_no_db_routes.py
+++ tests/test_no_db_routes.py
@@ -1,5 +1,13 @@
 """Run with no DB to simulate connection failure
 """
+import pytest
+import os
+import sys
+
+src_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if src_dir not in sys.path:
+    sys.path.append(src_dir)
+
 
 def test_db_connect_fail(flask_client):
     """Start with a database connection"""
@@ -9,32 +17,37 @@ def test_db_connect_fail(flask_client):
     rv = flask_client.get('/ping')
     assert rv.status_code == 500
 
-def test_wcs_fail(flask_client):
+def test_wcs_fail(monkeypatch, flask_client):
     """WCS endpoint fails"""
 
+    monkeypatch.setenv("DATACUBE_OWS_CFG", "tests.cfg.minimal_cfg.ows_cfg")
     rv = flask_client.get('/wcs')
     assert rv.status_code == 400
 
-def test_wms_fail(flask_client):
+def test_wms_fail(monkeypatch, flask_client):
     """WMS endpoint fails"""
 
+    monkeypatch.setenv("DATACUBE_OWS_CFG", "tests.cfg.minimal_cfg.ows_cfg")
     rv = flask_client.get('/wms')
     assert rv.status_code == 400
 
-def test_wmts_fail(flask_client):
+def test_wmts_fail(monkeypatch, flask_client):
     """WMTS endpoint fails"""
 
+    monkeypatch.setenv("DATACUBE_OWS_CFG", "tests.cfg.minimal_cfg.ows_cfg")
     rv = flask_client.get('/wmts')
     assert rv.status_code == 400
 
-def test_legend_fail(flask_client):
+def test_legend_fail(monkeypatch, flask_client):
     """Fail on legend"""
 
+    monkeypatch.setenv("DATACUBE_OWS_CFG", "tests.cfg.minimal_cfg.ows_cfg")
     rv = flask_client.get("/legend/layer/style/legend.png")
     assert rv.status_code == 404
 
-def test_index_fail(flask_client):
+def test_index_fail(monkeypatch, flask_client):
     """Base index endpoint fails"""
 
+    monkeypatch.setenv("DATACUBE_OWS_CFG", "tests.cfg.minimal_cfg.ows_cfg")
     rv = flask_client.get('/')
     assert rv.status_code == 200
