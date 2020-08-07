822ae91ea092d2da53aa8b68fd6b4bc6fad0eb03
phaesler <paul.haesler@data61.csiro.au>
2020-08-07 11:32:28 +1000

    Make unit tests more fun.

--- tests/test_mv_selopts.py
+++ tests/test_mv_selopts.py
@@ -1,28 +1,31 @@
 from datacube_ows.mv_index import MVSelectOpts
 
-def test_all():
-    stv = None
 
-    assert MVSelectOpts.ALL.sel(stv) == [None]
+def test_all():
+    assert MVSelectOpts.ALL.sel("Ni!!") == ["Ni!!"]
+
 
 class MockSTV:
     def __init__(self, id):
         self.id = id
         self.c = self
 
+
 def test_ids_datasets():
     class MockSTV:
         def __init__(self, id):
             self.id = id
             self.c = self
-    stv = MockSTV(72)
-    assert MVSelectOpts.IDS.sel(stv) == [72]
-    assert MVSelectOpts.DATASETS.sel(stv) == [72]
+    stv = MockSTV(42)
+    assert MVSelectOpts.IDS.sel(stv) == [42]
+    assert MVSelectOpts.DATASETS.sel(stv) == [42]
+
 
 def test_extent():
     sel= MVSelectOpts.EXTENT.sel(None)
     assert len(sel) == 1
     assert str(sel[0]) == "ST_AsGeoJSON(ST_Union(spatial_extent))"
+
 
 def test_count():
     from sqlalchemy import text
