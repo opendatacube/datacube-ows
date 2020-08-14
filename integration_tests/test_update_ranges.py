b20d1e5564e88a999506766ffa0ea63619a6c61b
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2020-08-14 00:21:09 +0000

    Add assertion for output

--- integration_tests/test_update_ranges.py
+++ integration_tests/test_update_ranges.py
@@ -7,24 +7,29 @@ from datacube_ows.update_ranges import main
 
 def test_updates_ranges_schema(runner):
     result = runner.invoke(main,["--schema","--role",os.getenv("DB_USERNAME")])
+    assert 'ERROR' not in result.output
     assert result.exit_code == 0
 
 def test_update_ranges_views(runner):
     result = runner.invoke(main,["--views","--blocking"])
+    assert 'ERROR' not in result.output
     assert result.exit_code == 0
 
     result = runner.invoke(main,["--views"])
+    assert 'ERROR' not in result.output
     assert result.exit_code == 0
 
 def test_update_version(runner):
     result = runner.invoke(main,["--version"])
+    assert 'ERROR' not in result.output
     assert result.exit_code == 0
 
 def test_update_ranges_product(runner):
     result = runner.invoke(main,["ls8_usgs_level1_scene_layer"])
-    assert result.output i= "ERROR"
+    assert 'ERROR' not in result.output
     assert result.exit_code == 0
 
 def test_update_ranges(runner):
     result = runner.invoke(main)
+    assert 'ERROR' not in result.output
     assert result.exit_code == 0
