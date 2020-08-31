17d64eb6d387409466c124ec7bc377cf07d4a7ad
phaesler <paul.haesler@data61.csiro.au>
2020-08-31 18:28:04 +1000

    So hard getting tests passing locally and on github.

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -4,10 +4,11 @@ from unittest.mock import patch
 import os
 
 def test_fake_creds(monkeypatch):
+    from datacube_ows.ogc import ows_init_libs
     monkeypatch.setenv("AWS_DEFAULT_REGION", "us-west-1")
     monkeypatch.setenv("AWS_NO_SIGN_REQUEST", "false")
-    with patch("datacube.utils.aws.configure_s3_access"):
-        from datacube_ows.ogc import ows_init_libs
+    with patch("datacube_ows.ogc.configure_s3_access") as s3a:
+        s3a.return_value = None
         ows_init_libs()
         assert os.getenv("AWS_NO_SIGN_REQUEST") == "NO"
         monkeypatch.setenv("AWS_NO_SIGN_REQUEST", "indubitably")
