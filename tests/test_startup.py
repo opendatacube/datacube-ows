89989c50134a6e78161c2c8fff11e505a4c17c4b
phaesler <paul.haesler@data61.csiro.au>
2020-08-31 16:51:12 +1000

    Test fix.

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -5,7 +5,10 @@ def test_fake_creds(monkeypatch):
     monkeypatch.setenv("AWS_DEFAULT_REGION", "us-west-1")
     monkeypatch.setenv("AWS_NO_SIGN_REQUEST", "false")
     from datacube_ows.ogc import ows_init_libs
-    ows_init_libs()
+    try:
+        ows_init_libs()
+    except ValueError:
+        pass
     assert os.getenv("AWS_NO_SIGN_REQUEST") == "NO"
     monkeypatch.setenv("AWS_NO_SIGN_REQUEST", "indubitably")
     ows_init_libs()
