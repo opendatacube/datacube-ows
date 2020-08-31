b706b2697ae3943c2d479a17b0dfc3775d508d68
phaesler <paul.haesler@data61.csiro.au>
2020-08-31 16:04:53 +1000

    Basic unit testing

---
+++ tests/test_startup.py
@@ -0,0 +1,12 @@
+import pytest
+import os
+
+def test_fake_creds(monkeypatch):
+    monkeypatch.setenv("AWS_DEFAULT_REGION", "us-west-1")
+    monkeypatch.setenv("AWS_NO_SIGN_REQUEST", "false")
+    from datacube_ows.ogc import ows_init_libs
+    ows_init_libs()
+    assert os.getenv("AWS_NO_SIGN_REQUEST") == "NO"
+    monkeypatch.setenv("AWS_NO_SIGN_REQUEST", "indubitably")
+    ows_init_libs()
+    assert os.getenv("AWS_ACCESS_KEY_ID") == "fake"
