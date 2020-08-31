b677f256af37a72c82d10f96538b7e4a12dd861e
phaesler <paul.haesler@data61.csiro.au>
2020-08-31 17:33:02 +1000

    Stop test from dying where no AWS credentials

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -1,15 +1,15 @@
 import pytest
+
+from unittest.mock import patch
 import os
 
 def test_fake_creds(monkeypatch):
     monkeypatch.setenv("AWS_DEFAULT_REGION", "us-west-1")
     monkeypatch.setenv("AWS_NO_SIGN_REQUEST", "false")
-    from datacube_ows.ogc import ows_init_libs
-    try:
+    with patch("datacube.utils.aws.configure_s3_access"):
+        from datacube_ows.ogc import ows_init_libs
         ows_init_libs()
-    except ValueError:
-        pass
-    assert os.getenv("AWS_NO_SIGN_REQUEST") == "NO"
-    monkeypatch.setenv("AWS_NO_SIGN_REQUEST", "indubitably")
-    ows_init_libs()
-    assert os.getenv("AWS_ACCESS_KEY_ID") == "fake"
+        assert os.getenv("AWS_NO_SIGN_REQUEST") == "NO"
+        monkeypatch.setenv("AWS_NO_SIGN_REQUEST", "indubitably")
+        ows_init_libs()
+        assert os.getenv("AWS_ACCESS_KEY_ID") == "fake"
