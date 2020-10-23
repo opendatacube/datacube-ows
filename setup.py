c8c3670dd7fef9c15e0e5c699bb3d7cfd1e2dcb7
phaesler <paul.haesler@data61.csiro.au>
2020-10-23 11:58:28 +1100

    Ahah - missing comma.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     entry_points={
         'console_scripts': [
             'datacube-ows=datacube_ows.wsgi:main',
-            'datacube-ows-update=datacube_ows.update_ranges:main'
+            'datacube-ows-update=datacube_ows.update_ranges:main',
             'datacube-ows-cfg-parse=datacube_ows.cfg_parser:main'
         ]
     },
