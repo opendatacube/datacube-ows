9ca693143099b648a2c3cc8e968f1d2470655f64
phaesler <paul.haesler@data61.csiro.au>
2021-03-24 16:19:51 +1100

    Learning how to disable checks in flake8.

--- datacube_ows/styles/api/__init__.py
+++ datacube_ows/styles/api/__init__.py
@@ -1,8 +1,6 @@
 
 from datacube_ows.styles.api.base import StandaloneStyle, \
     apply_ows_style, apply_ows_style_cfg, \
-    generate_ows_legend_style, generate_ows_legend_style_cfg
-import datacube_ows.ogc_utils as utils
+    generate_ows_legend_style, generate_ows_legend_style_cfg   # noqa: F401
+from datacube_ows.ogc_utils, create_geobox, xarray_image_as_png # noqa: F401
 
-create_geobox = utils.create_geobox
-xarray_image_as_png = utils.xarray_image_as_png
