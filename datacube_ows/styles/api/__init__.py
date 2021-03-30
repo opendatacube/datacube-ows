8fd85939d202f9b4bb720b6b50dd6ce574c79790
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-03-30 05:45:50 +0000

    apply isort

--- datacube_ows/styles/api/__init__.py
+++ datacube_ows/styles/api/__init__.py
@@ -1,9 +1,9 @@
 
 from datacube_ows.band_utils import (band_modulator, scalable,  # noqa: F401
                                      scale_data)
-from datacube_ows.ogc_utils import (create_geobox,  # noqa: F401
-                                    xarray_image_as_png)
-from datacube_ows.styles.api.base import (StandaloneStyle,  # noqa: F401
-                                          apply_ows_style, apply_ows_style_cfg,
+from datacube_ows.ogc_utils import create_geobox  # noqa: F401
+from datacube_ows.ogc_utils import xarray_image_as_png
+from datacube_ows.styles.api.base import StandaloneStyle  # noqa: F401
+from datacube_ows.styles.api.base import (apply_ows_style, apply_ows_style_cfg,
                                           generate_ows_legend_style,
                                           generate_ows_legend_style_cfg)
