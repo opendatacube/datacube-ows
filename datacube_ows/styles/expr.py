8c4c12097ead80a1e396469c0ec904e311113ee1
phaesler <paul.haesler@data61.csiro.au>
2021-04-13 14:56:30 +1000

    Remove unused (and therefore untested) code from expr.py.

--- datacube_ows/styles/expr.py
+++ datacube_ows/styles/expr.py
@@ -1,9 +1,4 @@
 import lark
-import numpy
-from datacube.utils.masking import valid_data_mask
-
-# N.B. Copied from datacube.virtual.expr
-# Remove this file and reference core directly after next ODC release.
 
 def formula_parser():
     return lark.Lark("""
@@ -63,77 +58,3 @@ def formula_parser():
 
                 %ignore WS_INLINE
                 """, start='expr')
-
-
-@lark.v_args(inline=True)
-class FormulaEvaluator(lark.Transformer):
-    from operator import (add, and_, eq, floordiv, ge, gt, inv, le, lshift, lt,
-                          mod, mul, ne, neg, not_, or_, pos, pow, rshift, sub,
-                          truediv, xor)
-
-    float_literal = float
-    int_literal = int
-
-
-@lark.v_args(inline=True)
-class MaskEvaluator(lark.Transformer):
-    # the result of an expression is nodata whenever any of its subexpressions is nodata
-    from operator import or_
-
-    # pylint: disable=invalid-name
-    and_ = _xor = or_
-    eq = ne = le = ge = lt = gt = or_
-    add = sub = mul = truediv = floordiv = mod = pow = lshift = rshift = or_
-
-    def not_(self, value):
-        return value
-
-    neg = pos = inv = not_
-
-    @staticmethod
-    def float_literal(value):
-        return False
-
-    @staticmethod
-    def int_literal(value):
-        return False
-
-
-def evaluate_type(formula, env, parser, evaluator):
-    """
-    Evaluates the type of the output of a formula given a parser,
-    a corresponding evaluator class, and an environment.
-    The environment is a dict-like object (such as an `xarray.Dataset`) that maps variable names to values.
-    """
-    @lark.v_args(inline=True)
-    class TypeEvaluator(evaluator):
-        def var_name(self, key):
-            return numpy.array([], dtype=env[key.value].dtype)
-
-    return TypeEvaluator().transform(parser.parse(formula))
-
-def evaluate_data(formula, env, parser, evaluator):
-    """
-    Evaluates a formula given a parser, a corresponding evaluator class, and an environment.
-    The environment is a dict-like object (such as an `xarray.Dataset`) that maps variable names to values.
-    """
-    @lark.v_args(inline=True)
-    class DataEvaluator(evaluator):
-        def var_name(self, key):
-            return env[key.value]
-
-    return DataEvaluator().transform(parser.parse(formula))
-
-
-def evaluate_nodata_mask(formula, env, parser, evaluator):
-    """
-    Evaluates the nodata mask for a formula given a parser, a corresponding evaluator class, and an environment.
-    The environment is a dict-like object (such as an `xarray.Dataset`) that maps variable names to values.
-    """
-    @lark.v_args(inline=True)
-    class NodataMaskEvaluator(evaluator):
-        def var_name(self, key):
-            # pylint: disable=invalid-unary-operand-type
-            return ~valid_data_mask(env[key.value])
-
-    return NodataMaskEvaluator().transform(parser.parse(formula))
