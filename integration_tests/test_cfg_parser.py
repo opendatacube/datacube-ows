27650e2f725a4864a118d1ecd7ae23914741f41e
phaesler <paul.haesler@data61.csiro.au>
2021-05-28 08:38:03 +1000

    More tests.

--- integration_tests/test_cfg_parser.py
+++ integration_tests/test_cfg_parser.py
@@ -80,7 +80,7 @@ def test_cfg_parser_bad_cfgenv(runner):
 
 
 def test_cfg_parser_bad_cfgarg(runner):
-    result = runner.invoke(main, ["check", "integration_tests.cfg.ows_test_cfg.ows,cfg", "integration_tests.cfg.ows_test_cfg_bad.ows_cfg"])
+    result = runner.invoke(main, ["check", "integration_tests.cfg.ows_test_cfg.ows_cfg", "integration_tests.cfg.ows_test_cfg_bad.ows_cfg"])
     assert result.exit_code == 1
 
 
@@ -88,4 +88,17 @@ def test_cfg_write_new_translation_directory(runner):
     this_dir = os.path.dirname(__file__)
     result = runner.invoke(main, ["translation", "-n", "-m", f"{this_dir}/cfg/message.po", "-d", f"{this_dir}/cfg/test_translations", "-D", "ows_cfg", "de"])
     assert result.exit_code == 0
+
+
+def test_cfg_write_new_translation_directory_all_langs(runner):
+    this_dir = os.path.dirname(__file__)
+    result = runner.invoke(main, ["translation", "-n", "-m", f"{this_dir}/cfg/message.po", "-d", f"{this_dir}/cfg/test_translations", "-D", "ows_cfg", "all"])
+    assert result.exit_code == 0
+
+
+def test_cfg_new_translation_no_language(runner):
+    this_dir = os.path.dirname(__file__)
+    result = runner.invoke(main, ["translation", "-n", "-m", f"{this_dir}/cfg/message.po", "-d", f"{this_dir}/cfg/test_translations", "-D", "ows_cfg"])
+    assert result.exit_code == 1
+
 
