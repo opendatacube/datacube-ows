ed193c87232f1e51db5a4ff1429c356273fbf4b5
phaesler <paul.haesler@data61.csiro.au>
2021-07-07 14:43:19 +1000

    Outgoing band translation tests.

--- integration_tests/test_i18n.py
+++ integration_tests/test_i18n.py
@@ -19,6 +19,17 @@ def test_wcs1_i18n(ows_server):
     # Confirm success
     assert "German translation" in resp.text
 
+
+def test_wcs1_bands_i18n(ows_server):
+    resp = requests.get(
+        ows_server.url + "/wcs?request=DescribeCoverage&service=WCS&version=1.0.0&coverageid=ls8_usgs_level1_scene_layer",
+        timeout=10,
+        headers={"Accept-Language": "de"}
+    )
+    # Confirm success
+    assert "gruen" in resp.text
+
+
 def test_wcs2_i18n(ows_server):
     resp = requests.get(
         ows_server.url + "/wcs?request=GetCapabilities&service=WCS&version=2.0.1",
@@ -27,3 +38,13 @@ def test_wcs2_i18n(ows_server):
     )
     # Confirm success
     assert "German translation" in resp.text
+
+
+def test_wcs1_bands_i18n(ows_server):
+    resp = requests.get(
+        ows_server.url + "/wcs?request=DescribeCoverage&service=WCS&version=2.0.1&coverageid=ls8_usgs_level1_scene_layer",
+        timeout=10,
+        headers={"Accept-Language": "de"}
+    )
+    # Confirm success
+    assert "gruen" in resp.text
