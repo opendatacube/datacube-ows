8873447da6b0726da0ebee659028da09ebf6303c
phaesler <paul.haesler@data61.csiro.au>
2021-07-12 19:46:51 +1000

    pytest gonna pytest.

--- tests/test_ows_configuration.py
+++ tests/test_ows_configuration.py
@@ -10,22 +10,22 @@ import pytest
 import datacube_ows.config_utils
 import datacube_ows.ogc_utils
 import datacube_ows.ows_configuration
-from tests.utils import test_function
+from tests.utils import a_function
 
 
 def test_function_wrapper_lyr():
     lyr = MagicMock()
-    func_cfg = "tests.utils.test_function"
+    func_cfg = "tests.utils.a_function"
     f = datacube_ows.ogc_utils.FunctionWrapper(lyr, func_cfg)
     assert f(7)[0] == "a7  b2  c3"
     assert f.band_mapper is None
     func_cfg = {
-        "function": "tests.utils.test_function",
+        "function": "tests.utils.a_function",
     }
     f = datacube_ows.ogc_utils.FunctionWrapper(lyr, func_cfg)
     assert f(7, 8)[0] == "a7  b8  c3"
     func_cfg = {
-        "function": "tests.utils.test_function",
+        "function": "tests.utils.a_function",
         "kwargs": {
             "foo": "bar",
             "c": "ouple"
@@ -42,16 +42,16 @@ def test_func_naked():
     lyr = MagicMock()
     with pytest.raises(datacube_ows.config_utils.ConfigException) as e:
         f = datacube_ows.ogc_utils.FunctionWrapper(lyr, {
-            "function": test_function,
+            "function": a_function,
         })
     assert str("Directly including callable objects in configuration is no longer supported.")
     with pytest.raises(datacube_ows.config_utils.ConfigException) as e:
-        f = datacube_ows.ogc_utils.FunctionWrapper(lyr, test_function)
+        f = datacube_ows.ogc_utils.FunctionWrapper(lyr, a_function)
     assert str("Directly including callable objects in configuration is no longer supported.")
     f = datacube_ows.ogc_utils.FunctionWrapper(lyr, {
-        "function": test_function,
+        "function": a_function,
     }, stand_alone=True)
     assert f("ardvark", "bllbbll")[0] == "aardvark  bbllbbll  c3"
-    f = datacube_ows.ogc_utils.FunctionWrapper(lyr, test_function, stand_alone=True)
+    f = datacube_ows.ogc_utils.FunctionWrapper(lyr, a_function, stand_alone=True)
     assert f("ardvark", "bllbbll")[0] == "aardvark  bbllbbll  c3"
 
--- tests/utils.py
+++ tests/utils.py
@@ -21,7 +21,7 @@ coords = [
 ]
 
 
-def test_function(a, b=2, c=3, **kwargs):
+def a_function(a, b=2, c=3, **kwargs):
     return [f"a{a}  b{b}  c{c}", kwargs]
 
 
