5873788f65454cf0df027b40c19eac3e0a7272c7
Tisham Dhar <whatnickd@gmail.com>
2021-07-15 15:54:57 +0930

    #656 Warning on experimental animation
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

--- datacube_ows/styles/base.py
+++ datacube_ows/styles/base.py
@@ -288,6 +288,9 @@ class StyleDefBase(OWSExtensibleConfigEntry, OWSMetadataConfig):
                 raise ConfigException("multi_date handler allowed_count_range: minimum must be less than equal to maximum")
 
             self.animate = cfg.get("animate", False)
+            if self.animate:
+                _LOG.warning("animations are experimental, use at your own risk")
+                
             if "aggregator_function" in cfg:
                 self.aggregator = FunctionWrapper(style.product, cfg["aggregator_function"])
             else:
