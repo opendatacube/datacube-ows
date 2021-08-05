08a5e43c3d1c5c44ecb1998cbea9c9d2c256798f
phaesler <paul.haesler@data61.csiro.au>
2021-08-05 11:37:59 +1000

    Type-hints and docstrings in resource_limits.py

--- datacube_ows/resource_limits.py
+++ datacube_ows/resource_limits.py
@@ -1,28 +1,39 @@
-from datacube_ows.config_utils import OWSConfigEntry
+from typing import List, Mapping, Optional, cast
+
+from datacube_ows.config_utils import OWSConfigEntry, CFG_DICT, RAW_CFG
 from datacube_ows.ogc_utils import ConfigException
 
 
 class CacheControlRules(OWSConfigEntry):
-    def __init__(self, cfg, context, max_datasets):
+    def __init__(self, cfg: RAW_CFG, context: str, max_datasets: int) -> None:
+        """
+        Class constructor. stores and validates rule dictionaries
+
+        :param cfg: None, or a list of progressive CCR configurations.
+        :param context: Context label (e.g. owning layer name), used for reporting validation errors
+        :param max_datasets: Over-arching maximum dataset limit in context.
+        """
         super().__init__(cfg)
-        self.rules = cfg
-        self.use_caching = self.rules is not None
+        self.rules = cast(Optional[List[CFG_DICT]], self._raw_cfg)
+        self.use_caching: bool = self.rules is not None
         self.max_datasets = max_datasets
         if not self.use_caching:
             return
-        min_so_far = 0
-        max_max_age_so_far = 0
-        for rule in self.rules:
+
+        # Validate rules
+        min_so_far: int = 0
+        max_max_age_so_far: int = 0
+        for rule in cast(List[CFG_DICT], self.rules):
             if "min_datasets" not in rule:
                 raise ConfigException(f"Dataset cache rule does not contain a 'min_datasets' element in {context}")
             if "max_age" not in rule:
                 raise ConfigException(f"Dataset cache rule does not contain a 'max_age' element in {context}")
-            min_datasets = rule["min_datasets"]
-            max_age = rule["max_age"]
-            if not isinstance(min_datasets, int):
+            if not isinstance(rule["min_datasets"], int):
                 raise ConfigException(f"Dataset cache rule has non-integer 'min_datasets' element in {context}")
-            if not isinstance(max_age, int):
+            min_datasets =  cast(int, rule["min_datasets"])
+            if not isinstance(rule["max_age"], int):
                 raise ConfigException(f"Dataset cache rule has non-integer 'max_age' element in {context}")
+            max_age = cast(int, rule["max_age"])
             if min_datasets <= 0:
                 raise ConfigException(f"Invalid dataset cache rule in {context}: min_datasets must be greater than zero.")
             if min_datasets <= min_so_far:
@@ -36,7 +47,13 @@ class CacheControlRules(OWSConfigEntry):
                 raise ConfigException(f"max_age values in dataset cache rules must increase monotonically in layer {context}.")
             max_max_age_so_far = max_age
 
-    def cache_headers(self, n_datasets):
+    def cache_headers(self, n_datasets: int) -> Mapping[str, str]:
+        """
+        Generate Cache Control headers for a request accessing a number of datasets.
+
+        :param n_datasets: number of datasets
+        :return: Dictionary containing the appropriate HTTP cache control response headers.
+        """
         if not self.use_caching:
             return {}
         assert n_datasets >= 0
@@ -53,20 +70,61 @@ class CacheControlRules(OWSConfigEntry):
             return {"cache-control": "no-cache"}
 
 
+class ResourceLimited(Exception):
+    def __init__(self, reasons: List[str]):
+        self.reasons = reasons
+        super().__init__(f"Resource limit(s) exceeded: {','.join(reasons)}")
+
+
 class OWSResourceManagementRules(OWSConfigEntry):
     # pylint: disable=attribute-defined-outside-init
-    def __init__(self, cfg, context):
+    def __init__(self, cfg: CFG_DICT, context: str) -> None:
+        """
+        Class constructor.
+
+        :param cfg: A resource limit configuration dictionary.
+        :param context: The context (e.g. layer name) for reporting validation errors.
+        """
         super().__init__(cfg)
-        cfg = self._raw_cfg
-        wms_cfg = cfg.get("wms", {})
-        wcs_cfg = cfg.get("wcs", {})
-        self.zoom_fill = wms_cfg.get("zoomed_out_fill_colour", [150, 180, 200, 160])
+        cfg = cast(CFG_DICT, self._raw_cfg)
+        wms_cfg = cast(CFG_DICT, cfg.get("wms", {}))
+        wcs_cfg = cast(CFG_DICT, cfg.get("wcs", {}))
+        self.zoom_fill = cast(List[int], wms_cfg.get("zoomed_out_fill_colour", [150, 180, 200, 160]))
         if len(self.zoom_fill) == 3:
             self.zoom_fill += [255]
         if len(self.zoom_fill) != 4:
             raise ConfigException(f"zoomed_out_fill_colour must have 3 or 4 elements in {context}")
-        self.min_zoom = wms_cfg.get("min_zoom_factor", 300.0)
-        self.max_datasets_wms = wms_cfg.get("max_datasets", 0)
-        self.max_datasets_wcs = wcs_cfg.get("max_datasets", 0)
+        self.min_zoom = cast(float, wms_cfg.get("min_zoom_factor", 300.0))
+        self.max_datasets_wms = cast(int, wms_cfg.get("max_datasets", 0))
+        self.max_datasets_wcs = cast(int, wcs_cfg.get("max_datasets", 0))
         self.wms_cache_rules = CacheControlRules(wms_cfg.get("dataset_cache_rules"), context, self.max_datasets_wms)
         self.wcs_cache_rules = CacheControlRules(wcs_cfg.get("dataset_cache_rules"), context, self.max_datasets_wcs)
+
+    def check_wms(self, n_datasets: int, zoom_factor: float) -> None:
+        """
+        Check whether a WMS requests exceeds the configured resource limits.
+
+        :param n_datasets: The number of datasets for the query
+        :param zoom_factor: The zoom factor of the query
+        :raises: ResourceLimited if any limits are exceeded.
+        """
+        limits_exceeded: List[str] = []
+        if self.max_datasets_wms > 0 and n_datasets > self.max_datasets_wms:
+            limits_exceeded.append("too many datasets")
+        if zoom_factor < self.min_zoom:
+            limits_exceeded.append("zoomed out too far")
+        if limits_exceeded:
+            raise ResourceLimited(limits_exceeded)
+
+    def check_wcs(self, n_datasets: int) -> None:
+        """
+        Check whether a WCS requests exceeds the configured resource limits.
+
+        :param n_datasets: The number of datasets for the query
+        :raises: ResourceLimited if any limits are exceeded.
+        """
+        limits_exceeded: List[str] = []
+        if self.max_datasets_wcs > 0 and n_datasets > self.max_datasets_wcs:
+            limits_exceeded.append("too many datasets")
+        if limits_exceeded:
+            raise ResourceLimited(limits_exceeded)
