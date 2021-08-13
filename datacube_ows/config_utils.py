58510c2912acae14c44eaf091af13adb9d60435e
phaesler <paul.haesler@data61.csiro.au>
2021-08-13 13:07:21 +1000

    Add better error messages for config import errors.

--- datacube_ows/config_utils.py
+++ datacube_ows/config_utils.py
@@ -122,7 +122,12 @@ def import_python_obj(path: str) -> RAW_CFG:
     :return: a Python object, or None
     """
     mod_name, obj_name = path.rsplit('.', 1)
-    mod = import_module(mod_name)
+    try:
+        mod = import_module(mod_name)
+    except ImportError:
+        raise ConfigException(f"Could not import python object: {path}")
+    except ValueError:
+        raise ConfigException(f"Could not import python object: {path}")
     obj = getattr(mod, obj_name)
     return cast(RAW_CFG, obj)
 
