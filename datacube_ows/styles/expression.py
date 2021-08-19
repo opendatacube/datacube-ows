3fb9cf91c4217d5183ca69e943683a806c0a6b50
phaesler <paul.haesler@data61.csiro.au>
2021-08-19 13:48:56 +1000

    Lint, flake8, isort

--- datacube_ows/styles/expression.py
+++ datacube_ows/styles/expression.py
@@ -4,6 +4,7 @@
 # Copyright (c) 2017-2021 OWS Contributors
 # SPDX-License-Identifier: Apache-2.0
 from typing import Any, Type, cast
+
 import lark
 from datacube.virtual.expr import formula_parser
 
