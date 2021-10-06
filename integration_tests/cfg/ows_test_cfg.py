bd38627249efc1ac7e886a7a5f22b1f581242ff1
Paul Haesler <linkjuggler@gmail.com>
2021-10-06 11:24:45 +1100

    Attempt simple test.

--- integration_tests/cfg/ows_test_cfg.py
+++ integration_tests/cfg/ows_test_cfg.py
@@ -731,6 +731,9 @@ ows_cfg = {
                     "abstract": "Imagery from the Level 1 Landsat-8 USGS Public Data Set Clone",
                     "name": "ls8_usgs_level1_scene_layer_clone",
                     "low_res_product_name": "ls8_usgs_level1_scene",
+                    "image_processing": {
+                        "extent_mask_func": []
+                    }
                     "resource_limits": {
                         "wcs": {
                             "max_image_size": 2000 * 2000 * 3 * 2,
