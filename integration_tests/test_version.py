830093935f6c29bca46b66a4c1363f9123691d8c
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2021-10-11 23:50:28 +0000

    add version test

---
+++ integration_tests/test_version.py
@@ -0,0 +1,15 @@
+# This file is part of datacube-ows, part of the Open Data Cube project.
+# See https://opendatacube.org for more information.
+#
+# Copyright (c) 2017-2021 OWS Contributors
+# SPDX-License-Identifier: Apache-2.0
+"""Test update ranges on DB using Click testing
+https://click.palletsprojects.com/en/7.x/testing/
+"""
+from datacube_ows.update_ranges_impl import main
+from datacube_ows.__init__ import __version__
+
+def test_updates_ranges_schema(runner, role_name):
+    result = runner.invoke(main, ["--version])
+    assert __version__ in result.output
+    assert result.exit_code == 0
