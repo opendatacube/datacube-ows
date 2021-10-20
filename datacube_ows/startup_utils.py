3a778df5e23ac708c6b45b93b57ee4e85d481f1c
Paul Haesler <linkjuggler@gmail.com>
2021-10-21 09:25:08 +1100

    Correct name/label.

--- datacube_ows/startup_utils.py
+++ datacube_ows/startup_utils.py
@@ -200,7 +200,7 @@ def initialise_prometheus_register(metrics):
     if os.environ.get("prometheus_multiproc_dir", False):
         metrics.register_default(
             metrics.summary(
-                'flask_ows_request_full_url', 'Request summary by request url',
+                'flask_ows_request_method_and_layer', 'Request summary by protocol, version, command and layer',
                 labels={
                     'query_request': lambda: request.args.get('request'),
                     'query_service': lambda: request.args.get('service'),
