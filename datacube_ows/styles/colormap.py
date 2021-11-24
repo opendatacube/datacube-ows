59a22712e47e3f637572da93552253d16c9ce673
Paul Haesler <linkjuggler@gmail.com>
2021-11-24 12:02:53 +1100

    First attempt at generic matrix API for value map delta.

--- datacube_ows/styles/colormap.py
+++ datacube_ows/styles/colormap.py
@@ -52,11 +52,7 @@ class ValueMapRule(OWSConfigEntry):
             self.label = None
         self.color_str = cast(str, cfg["color"])
         self.rgb = Color(self.color_str)
-        if cfg.get("mask", False):
-            self.alpha: float = 0.0
-        else:
-            self.alpha = float(cast(Union[float, int, str], cfg.get("alpha", 1.0)))
-
+        self.alpha = float(cast(Union[float, int, str], cfg.get("alpha", 1.0)))
         if "flags" in cfg:
             flags = cast(CFG_DICT, cfg["flags"])
             self.or_flags: bool = False
@@ -126,6 +122,151 @@ class ValueMapRule(OWSConfigEntry):
         return vmap
 
 
+class MultiDateValueMapRule(OWSConfigEntry):
+    # TODO: inherit from ValueMapRule? or merge into it?
+    """
+    A  Mulit-Date Value Map Rule.
+
+    Construct a Multi-Date ValueMap rule-set with MultiDateValueMapRule.value_map_from_config
+    """
+    def __init__(self, mdh: "ColorMapStyleDef.MultiDateHandler", band: str,
+                 rule_id: int, cfg: CFG_DICT) -> None:
+        """
+        Construct a Multi-date Value Map Rule
+
+        :param style_def: The owning ColorMapStyleDef object
+        :param band: The name of the flag-band the rules apply to
+        :param cfg: The rule specification
+        """
+        super().__init__(cfg)
+        cfg = cast(CFG_DICT, self._raw_cfg)
+        self.mdh = mdh
+        self.id = rule_id
+        self.band = mdh.style.local_band(band)
+
+        self.title = cast(str, cfg["title"])
+        self.abstract = cast(str, cfg.get("abstract"))
+        if self.title and self.abstract:
+            self.label: Optional[str] = f"{self.title} - {self.abstract}"
+        elif self.title:
+            self.label = self.title
+        elif self.abstract:
+            self.label = self.abstract
+        else:
+            self.label = None
+        self.color_str = cast(str, cfg["color"])
+        self.rgb = Color(self.color_str)
+        self.alpha = float(cast(Union[float, int, str], cfg.get("alpha", 1.0)))
+
+        self.flags: Optional[List[CFG_DICT]] = []
+        self.or_flags: Optional[List[bool]] = []
+        self.values: Optional[List[List[int]]] = []
+        if "flags" in cfg:
+            date_flags = cast(CFG_DICT, cfg["flags"])
+            if len(date_flags) != len(self.mdh.max_count):
+                pass
+            for flags in date_flags:
+                or_flag: bool = False
+                if "or" in flags and "and" in flags:
+                    raise ConfigException(f"MultiDateValueMap rule in style {self.mdh.style.name} of layer {self.mdh.style.product.name} combines 'and' and 'or' rules")
+                elif "or" in flags:
+                    or_flag = True
+                    flags = cast(CFG_DICT, flags["or"])
+                elif "and" in flags:
+                    flags = cast(CFG_DICT, flags["and"])
+                self.flags.append(flags)
+                self.or_flags.append(or_flag)
+        if "values" in cfg:
+            self.values = cast(List[List[int]], list(cfg["values"]))
+        else:
+            self.values = None
+        if not self.flags and not self.values:
+            raise ConfigException(f"Multi-Date Value map rule in style {mdh.style.name} of layer {mdh.style.product.name} must have a non-empty 'flags' or 'values' section.")
+        if self.flags and self.values:
+            raise ConfigException(f"Multi-Date Value map rule in style {mdh.style.name} of layer {mdh.style.product.name} has both a 'flags' and a 'values' section - choose one.")
+
+    def create_mask(self, data: DataArray) -> DataArray:
+        """
+        Create a mask from raw flag band data.
+
+        :param data: Multi-date Raw flag data, assumed to be for this rule's flag band.
+        :return: A boolean dateless DataArray, True where the data matches this rule
+        """
+        date_slices = (data.sel(time=dt) for dt in data.coords["time"].values)
+        mask: Optional[DataArray] = None
+        if self.values:
+            for d_slice, vals in zip(date_slices, self.values):
+                d_mask: Optional[DataArray] = None
+                for v in cast(List[int], vals):
+                    vmask = d_slice == v
+                    if d_mask is None:
+                        d_mask = vmask
+                    else:
+                        d_mask |= vmask
+                if mask is None:
+                    mask = d_mask
+                else:
+                    mask &= d_mask
+        else:
+            for d_slice, flags, or_flags in zip(date_slices, self.flags, self.or_flags):
+                d_mask: Optional[DataArray] = None
+                if or_flags:
+                    for f in cast(CFG_DICT, self.flags).items():
+                        f = {f[0]: f[1]}
+                        if d_mask is None:
+                            d_mask = make_mask(d_slice, **f)
+                        else:
+                            d_mask |= make_mask(d_slice, **f)
+                else:
+                    d_mask = make_mask(d_slice, **cast(CFG_DICT, self.flags))
+                if mask is None:
+                    mask = d_mask
+                else:
+                    mask &= d_mask
+        return mask
+
+    @classmethod
+    def value_map_from_config(cls,
+                              mdh: "ColorMapStyleDef.MultiDateHandler",
+                              cfg: CFG_DICT) -> MutableMapping[str, List["MultiDateValueMapRule"]]:
+        """
+        Create a multi-date value map rule set from a config specification
+
+        :param style: The parent style definition object
+        :param cfg: The specification for the multi-date value map.
+
+        :return: A value map ruleset dictionary.
+        """
+        vmap: MutableMapping[str, List["ValueMapRule"]] = {}
+        if mdh.min_count != mdh.max_count:
+            raise ConfigException("MultiDate value map only supported on multi-date handlers with min_count and max_count equal.")
+        for i, (band_name, rules) in enumerate(cfg.items()):
+            band_rules = [cls(mdh, band_name, i, rule) for rule in cast(List[CFG_DICT], rules)]
+            vmap[band_name] = band_rules
+        return vmap
+
+
+def apply_multidate_value_map(value_map: MutableMapping[str, List[MultiDateValueMapRule]],
+                    data: Dataset,
+                    band_mapper: Callable[[str], str]) -> Dataset:
+    imgdata = Dataset(coords={k: v for k, v in data.coords.items() if k != "time"})
+    for cfg_band, rules in value_map.items():
+        # Run through each item
+        band = band_mapper(cfg_band)
+        bdata = cast(DataArray, data[band])
+        band_data = Dataset()
+        if bdata.dtype.kind == 'f':
+            # Convert back to int for bitmasking
+            bdata = ColorMapStyleDef.reint(bdata)
+        for rule in rules:
+            mask = rule.create_mask(bdata)
+            masked = ColorMapStyleDef.create_colordata(bdata, rule.rgb, rule.alpha, mask)
+            band_data = masked if len(band_data.data_vars) == 0 else band_data.combine_first(masked)
+        imgdata = band_data if len(imgdata.data_vars) == 0 else merge([imgdata, band_data])
+    imgdata *= 255
+    return imgdata.astype('uint8')
+
+
 def apply_value_map(value_map: MutableMapping[str, List[ValueMapRule]],
                     data: Dataset,
                     band_mapper: Callable[[str], str]) -> Dataset:
@@ -140,7 +281,6 @@ def apply_value_map(value_map: MutableMapping[str, List[ValueMapRule]],
             bdata = ColorMapStyleDef.reint(bdata)
         for rule in rules:
             mask = rule.create_mask(bdata)
-
             masked = ColorMapStyleDef.create_colordata(bdata, rule.rgb, rule.alpha, mask)
             band_data = masked if len(band_data.data_vars) == 0 else band_data.combine_first(masked)
 
@@ -249,6 +389,7 @@ class ColorMapStyleDef(StyleDefBase):
 
     class MultiDateHandler(StyleDefBase.MultiDateHandler):
         auto_legend = True
+        non_animate_requires_aggregator = False
 
         def __init__(self, style: "ColorMapStyleDef", cfg: CFG_DICT) -> None:
             """
@@ -259,13 +400,18 @@ class ColorMapStyleDef(StyleDefBase):
             """
             super().__init__(style, cfg)
             style_cfg = cast(CFG_DICT, self._raw_cfg)
+            if self.animate:
+                self._value_map: Optional[MutableMapping[str, Union[ValueMapRule, MultiDateValueMapRule]]] = None
+            elif self.aggregator:
+                self._value_map = ValueMapRule.value_map_from_config(self.style, cast(CFG_DICT, self._raw_cfg["value_map"]))
+            else:
+                self._value_map = MultiDateValueMapRule.value_map_from_config(self, cast(CFG_DICT, self._raw_cfg["value_map"]))
 
         @property
         def value_map(self):
-            if self.animate:
-                return self.style.value_map
-            else:
-                return ValueMapRule.value_map_from_config(self.style, cast(CFG_DICT, self._raw_cfg["value_map"]))
+            if self._value_map is None:
+                self._value_map = self.style.value_map
+            return self._value_map
 
         def transform_data(self, data: "xarray.Dataset") -> "xarray.Dataset":
             """
@@ -274,8 +420,19 @@ class ColorMapStyleDef(StyleDefBase):
             :param data: Raw data
             :return: RGBA image xarray.  May have a time dimension
             """
-            agg = self.aggregator(data)
-            return apply_value_map(self.value_map, agg, self.style.product.band_idx.band)
+            if self.aggregator is None:
+                return apply_multidate_value_map(self.value_map, data, self.style.product.band_idx.band)
+            else:
+                agg = self.aggregator(data)
+                return apply_value_map(self.value_map, agg, self.style.product.band_idx.band)
+
+        def legend(self, bytesio: io.BytesIO) -> None:
+            """
+            Write a legend into a bytes buffer as a PNG image.
+
+            :param bytesio:  io.BytesIO byte buffer.
+            """
+            value_map_legend(self.value_map, self.legend_cfg, bytesio)
 
 # Register ColorMapStyleDef as a style subclass.
 StyleDefBase.register_subclass(ColorMapStyleDef, "value_map")
