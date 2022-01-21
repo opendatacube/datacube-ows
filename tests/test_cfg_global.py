bb1a13a8892addb40d91f915eb71380935e372ab
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-01-21 04:56:02 +0000

    setup internationalised unit test

--- tests/test_cfg_global.py
+++ tests/test_cfg_global.py
@@ -213,3 +213,20 @@ def test_no_langs(minimal_global_raw_cfg, minimal_dc):
     with pytest.raises(ConfigException) as excinfo:
         cfg = OWSConfig(cfg=minimal_global_raw_cfg)
     assert "at least one language" in str(excinfo.value)
+
+
+def test_two_langs(minimal_global_raw_cfg, minimal_dc):
+    OWSConfig._instance = None
+    minimal_global_raw_cfg["global"]["supported_languages"] = ["fr", "en"]
+    cfg = OWSConfig(cfg=minimal_global_raw_cfg)
+
+    assert cfg.global_config().default_locale == 'fr'
+    assert len(cfg.global_config().locales) == 2
+    assert cfg.global_config().internationalised is None
+
+def test_internationalised(minimal_global_raw_cfg, minimal_dc):
+    minimal_global_raw_cfg["global"]["supported_languages"] = ["fr", "en"]
+    minimal_global_raw_cfg["global"]["translations_directory"]= "/integration_tests/cfg/translations"
+    cfg = OWSConfig(cfg=minimal_global_raw_cfg)
+
+    assert cfg.global_config().internationalised is True
