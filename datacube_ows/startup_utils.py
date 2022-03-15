a30c58111a5f9a5980cb5ed278018be8b7daa62c
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-03-15 06:24:33 +0000

    fix formtting

--- datacube_ows/startup_utils.py
+++ datacube_ows/startup_utils.py
@@ -12,16 +12,15 @@ import warnings
 import sentry_sdk
 from botocore.credentials import RefreshableCredentials
 from datacube.utils.aws import configure_s3_access
-from flask import Flask, request, has_request_context
-
+from flask import Flask, has_request_context, request
 from flask_babel import Babel
 from flask_log_request_id import RequestID, RequestIDLogFilter
-from prometheus_flask_exporter.multiprocess import \
-    GunicornInternalPrometheusMetrics
 from rasterio.errors import NotGeoreferencedWarning
 from sentry_sdk.integrations.flask import FlaskIntegration
 
 from datacube_ows.ows_configuration import get_config
+from prometheus_flask_exporter.multiprocess import \
+    GunicornInternalPrometheusMetrics
 
 __all__ = [
     'initialise_babel',
