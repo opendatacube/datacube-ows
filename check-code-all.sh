29e15f82279c57c317c60e9fb082a26c772370cf
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-05-10 03:17:32 +0000

    replace multi-product test product

--- check-code-all.sh
+++ check-code-all.sh
@@ -8,16 +8,11 @@ sh ./docker/ows/wait-for-db
 # Users can specify extra folders as arguments.
 
 # On top of base sql sample, index two more product for multiproducts
-datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/inland_water/wofs/wofs_albers.yaml
-datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/land_and_vegetation/fc/ls5_fc_albers.yaml
-datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/land_and_vegetation/fc/ls7_fc_albers.yaml
-datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/land_and_vegetation/fc/ls8_fc_albers.yaml
+datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/baseline_satellite_data/definitive/s2a_ard_granule.odc-product.yaml
+datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/baseline_satellite_data/definitive/s2b_ard_granule.odc-product.yaml
 
-datacube dataset add https://data.dea.ga.gov.au/WOfS/WOFLs/v2.1.5/combined/x_6/y_-29/2004/05/03/LS_WATER_3577_6_-29_20040503003241500000_v1526732475.yaml
-datacube dataset add https://data.dea.ga.gov.au/fractional-cover/fc/v2.2.1/ls5/x_6/y_-29/2004/05/11/LS5_TM_FC_3577_6_-29_20040511002356.yaml
-datacube dataset add https://data.dea.ga.gov.au/fractional-cover/fc/v2.2.1/ls7/x_6/y_-29/2004/05/19/LS7_ETM_FC_3577_6_-29_20040519003242.yaml
-datacube dataset add https://data.dea.ga.gov.au/fractional-cover/fc/v2.2.1/ls8/x_6/y_-29/2018/05/27/LS8_OLI_FC_3577_6_-29_20180527003612.yaml
-
+datacube dataset add https://data.dea.ga.gov.au/baseline/s2a_ard_granule/2017-11-09/S2A_OPER_MSI_ARD_TL_SGS__20171109T022112_A012440_T54KVF_N02.06/eo3-ARD-METADATA.odc-metadata.yaml
+datacube dataset add https://data.dea.ga.gov.au/baseline/s2b_ard_granule/2017-11-09/S2B_OPER_MSI_ARD_TL_EPAE_20171109T165539_A003531_T56LLM_N02.06/eo3-ARD-METADATA.odc-metadata.yaml
 
 # create material view for ranges extents
 datacube-ows-update --schema --role $DB_USERNAME
