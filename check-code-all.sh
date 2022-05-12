1e61685a7cb768d472e5e1e9947e38feb9a89803
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-05-12 00:54:33 +0000

    index geodata for multiprod coverage

--- check-code-all.sh
+++ check-code-all.sh
@@ -18,10 +18,13 @@ datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-c
 datacube product add https://explorer.dev.dea.ga.gov.au/products/geodata_coast_100k.odc-product.yaml
 datacube product add https://raw.githubusercontent.com/GeoscienceAustralia/dea-config/master/products/inland_water/c3_wo/ga_ls_wo_3.odc-product.yaml
 
-datacube dataset add https://data.dea.ga.gov.au/baseline/s2a_ard_granule/2017-11-09/S2A_OPER_MSI_ARD_TL_SGS__20171109T022112_A012440_T54KVF_N02.06/eo3-ARD-METADATA.yaml
-datacube dataset add https://data.dea.ga.gov.au/baseline/s2b_ard_granule/2017-11-09/S2B_OPER_MSI_ARD_TL_EPAE_20171109T165539_A003531_T56LLM_N02.06/eo3-ARD-METADATA.yaml
-datacube dataset add https://data.dea.ga.gov.au/baseline/s2a_ard_granule/2017-08-24/S2A_OPER_MSI_ARD_TL_SGS__20170824T041752_A011339_T53KPA_N02.05/eo3-ARD-METADATA.yaml
-datacube dataset add https://data.dea.ga.gov.au/baseline/s2b_ard_granule/2017-08-24/S2B_OPER_MSI_ARD_TL_SGS__20170824T014339_A002430_T56KKD_N02.05/eo3-ARD-METADATA.yaml
+datacube dataset add https://data.dea.ga.gov.au/baseline/s2a_ard_granule/2017-11-09/S2A_OPER_MSI_ARD_TL_SGS__20171109T022112_A012440_T54KUD_N02.06/eo3-ARD-METADATA.odc-metadata.yaml
+datacube dataset add https://data.dea.ga.gov.au/baseline/s2b_ard_granule/2017-11-09/S2B_OPER_MSI_ARD_TL_EPAE_20171109T165835_A003531_T55HFA_N02.06/eo3-ARD-METADATA.odc-metadata.yaml
+datacube dataset add https://data.dea.ga.gov.au/baseline/s2a_ard_granule/2017-08-21/S2A_OPER_MSI_ARD_TL_SGS__20170821T040758_A011296_T54KUD_N02.05/eo3-ARD-METADATA.odc-metadata.yaml
+datacube dataset add https://data.dea.ga.gov.au/baseline/s2b_ard_granule/2017-08-21/S2B_OPER_MSI_ARD_TL_SGS__20170821T031444_A002387_T55HFA_N02.05/eo3-ARD-METADATA.odc-metadata.yaml
+
+datacube dataset add https://data.dea.ga.gov.au/projects/geodata_coast_100k/v2004/x_15/y_-40/COAST_100K_15_-40.yaml
+datacube dataset add https://data.dea.ga.gov.au/projects/geodata_coast_100k/v2004/x_8/y_-21/COAST_100K_8_-21.yaml
 
 datacube dataset add https://data.dea.ga.gov.au/derivative/ga_ls_wo_3/1-6-0/094/077/2018/02/08/ga_ls_wo_3_094077_2018-02-08_final.odc-metadata.yaml --confirm-ignore-lineage
 datacube dataset add https://data.dea.ga.gov.au/derivative/ga_ls_fc_3/2-5-1/094/077/2018/02/08/ga_ls_fc_3_094077_2018-02-08_final.odc-metadata.yaml --confirm-ignore-lineage
