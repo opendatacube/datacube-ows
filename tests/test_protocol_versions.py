aa6005e3c2cdc0f0fc82a90084afcbb7c25f4c59
Paul Haesler <linkjuggler@gmail.com>
2022-07-13 17:18:27 +1000

    Check for invalid OGC version parameters and make them valid.

--- tests/test_protocol_versions.py
+++ tests/test_protocol_versions.py
@@ -23,6 +23,7 @@ def supported_service():
 def test_default_exception(supported_service):
     assert supported_service.default_exception_class == TestException2
 
+
 def test_version_negotiation(supported_service):
     assert supported_service.negotiated_version("1.0").version == "1.2.7"
     assert supported_service.negotiated_version("1.2").version == "1.2.7"
@@ -34,3 +35,11 @@ def test_version_negotiation(supported_service):
     assert supported_service.negotiated_version("1.13.100").version == "1.13.0"
     assert supported_service.negotiated_version("2.0").version == "2.0.0"
     assert supported_service.negotiated_version("2.7.22").version == "2.0.0"
+
+
+def test_version_cleaner(supported_service):
+    assert supported_service._clean_version_parts(["0", "1", "2"]) == [0, 1, 2]
+    assert supported_service._clean_version_parts(["0", "1", "2/spam"]) == [0, 1, 2]
+    assert supported_service._clean_version_parts(["0", "1spam", "2"]) == [0, 1]
+    assert supported_service._clean_version_parts(["0?bacon", "1/eggs", "2/spam"]) == [0]
+    assert supported_service._clean_version_parts(["spam", "spam", "spam"]) == []
