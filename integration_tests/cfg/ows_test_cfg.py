ab54958a4840cf55552ad6db6d75d1fc96ed75c7
Paul Haesler <linkjuggler@gmail.com>
2023-11-13 09:17:42 +1100

    Update all license file-headers.

--- integration_tests/cfg/ows_test_cfg.py
+++ integration_tests/cfg/ows_test_cfg.py
@@ -2,7 +2,7 @@
 # This file is part of datacube-ows, part of the Open Data Cube project.
 # See https://opendatacube.org for more information.
 #
-# Copyright (c) 2017-2021 OWS Contributors
+# Copyright (c) 2017-2023 OWS Contributors
 # SPDX-License-Identifier: Apache-2.0
 
 import os
--- integration_tests/conftest.py
+++ integration_tests/conftest.py
@@ -1,8 +1,7 @@
-# isort: skip_file
 # This file is part of datacube-ows, part of the Open Data Cube project.
 # See https://opendatacube.org for more information.
 #
-# Copyright (c) 2017-2021 OWS Contributors
+# Copyright (c) 2017-2023 OWS Contributors
 # SPDX-License-Identifier: Apache-2.0
 
 import os
