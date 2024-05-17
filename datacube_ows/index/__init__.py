c922aa747341cd4065bb0adf68d867a438a80635
Paul Haesler <linkjuggler@gmail.com>
2024-05-17 12:55:55 +1000

    Add license headers to new files.

--- datacube_ows/index/__init__.py
+++ datacube_ows/index/__init__.py
@@ -1,3 +1,9 @@
+# This file is part of datacube-ows, part of the Open Data Cube project.
+# See https://opendatacube.org for more information.
+#
+# Copyright (c) 2017-2024 OWS Contributors
+# SPDX-License-Identifier: Apache-2.0
+
 from .api import ows_index, AbortRun, CoordRange, LayerSignature, LayerExtent
 
 
