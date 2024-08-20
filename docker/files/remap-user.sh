4869657368d07f1504e82778b05d4c6ca12aa0d6
Paul Haesler <linkjuggler@gmail.com>
2024-08-20 12:33:21 +1000

    ows user is not 1000, it's 1001?

--- docker/files/remap-user.sh
+++ docker/files/remap-user.sh
@@ -1,13 +1,14 @@
 #!/bin/bash -e
 
 # Script that gives the container user uid $LOCAL_UID and gid $LOCAL_GID.
-# If $LOCAL_UID or $LOCAL_GID are not set, they default to 1000 (default
+# If $LOCAL_UID or $LOCAL_GID are not set, they default to 1001 (default
 # for the first user created in Ubuntu).
 
-USER_ID=${LOCAL_UID:-1000}
-GROUP_ID=${LOCAL_GID:-1000}
+USER_ID=${LOCAL_UID:-1001}
+GROUP_ID=${LOCAL_GID:-1001}
+OWS_ID=$(id -u ows)
 
-[[ "$USER_ID" == "1000" ]] || usermod -u $USER_ID -o -m -d /home/ows ows
-[[ "$GROUP_ID" == "1000" ]] || groupmod -g $GROUP_ID ows
-[[ $(id -u) != "0" ]] || GOSU="/usr/sbin/gosu ubuntu"
+[[ "$USER_ID" == "$OWS_ID" ]] || usermod -u $USER_ID -o -m -d /home/ows ows
+[[ "$GROUP_ID" == "$OWS_ID" ]] || groupmod -g $GROUP_ID ows
+[[ $(id -u) != "0" ]] || GOSU="/usr/sbin/gosu ows"
 exec /usr/bin/tini -- $GOSU "$@"
