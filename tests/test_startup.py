d2a6212ce0d6c3a01b7c8b75e24c085f4d02754c
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2025-03-26 18:33:48 +0100

    Use non-deprecated PROMETHEUS_MULTIPROC_DIR
    The variable has been renamed to
    the upper case variable with the same
    name, so switch to using that name.
    This fixes some deprecation warnings
    in the tests.

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -111,7 +111,7 @@ def test_initialise_sentry(monkeypatch):
 
 
 def test_prometheus_inactive(monkeypatch):
-    monkeypatch.setenv("prometheus_multiproc_dir", "")
+    monkeypatch.setenv("PROMETHEUS_MULTIPROC_DIR", "")
     from datacube_ows.startup_utils import initialise_prometheus
     initialise_prometheus(None)
 
