d12d83bbf6708f356f9425bb76feb077cafc3939
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2025-03-26 13:37:04 +0100

    wsgi: stop crashing without config
    Datacube 1.9 throws an exception
    when pointed to a config file
    it cannot read, so only set
    the environment variable if the file
    exists.

--- datacube_ows/wsgi.py
+++ datacube_ows/wsgi.py
@@ -13,7 +13,8 @@ import sys
 sys.path.append("/opt")
 
 # The location of the datcube config file.
-os.environ.setdefault("ODC_CONFIG_PATH", "/opt/odc/.datacube.conf.local")
+if os.path.isfile("/opt/odc/.datacube.conf.local"):
+    os.environ.setdefault("ODC_CONFIG_PATH", "/opt/odc/.datacube.conf.local")
 
 from datacube_ows import __version__
 
