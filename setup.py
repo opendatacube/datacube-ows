e9b3f4b5259f67ec1685501b33e8f969af42679c
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2025-05-06 10:41:56 +0200

    Remove pep8 dependency
    This package has been renamed
    to pycodestyle and hasn't had
    a release since 2017, so remove
    it.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ install_requirements = [
 test_requirements = [
     'pytest', 'pytest_cov', 'pytest_localserver',
     'owslib>0.29.2',
-    'pytest_mock', 'pep8',
+    'pytest_mock',
     'pytest-helpers-namespace', 'flask-cors',
     'fsspec',
 ]
