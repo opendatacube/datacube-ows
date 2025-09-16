7fecf1a72d001872fcbf566cf1d6cccf6b2b3b7e
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2025-09-16 20:00:45 +0200

    Update to Ruff 0.13.0

--- datacube_ows/startup_utils.py
+++ datacube_ows/startup_utils.py
@@ -68,7 +68,7 @@ SentryEvent = TypeVar("SentryEvent")
 
 def before_send(event: SentryEvent, hint: dict[str, Any]) -> SentryEvent | None:
     if "exc_info" in hint:
-        exc_type, exc_value, tb = hint["exc_info"]
+        _, exc_value, _ = hint["exc_info"]
         if isinstance(
             exc_value, AttributeError
         ) and "object has no attribute 'GEOSGeom_destroy'" in str(exc_value):
