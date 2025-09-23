e80c55bb2b6cf9fb4066b9ba78b6323df802e57d
Ariana Barzinpour <ariana.barzinpour@ga.gov.au>
2025-09-23 04:03:08 +0000

    move comment

--- datacube_ows/ows_configuration.py
+++ datacube_ows/ows_configuration.py
@@ -1002,9 +1002,8 @@ class OWSNamedLayer(OWSExtensibleConfigEntry, OWSLayer):
         )
         # Native CRS
         if product_native_specs is not None and "crs" in product_native_specs:
-            self.native_CRS = str(
-                CRS(product_native_specs["crs"])
-            )  # normalise CRS string to avoid case difference issues
+            # normalise CRS string to avoid case difference issues
+            self.native_CRS = str(CRS(product_native_specs["crs"]))
             if self.cfg_native_crs == self.native_CRS:
                 _LOG.debug(
                     "Native crs for layer %s is specified in ODC metadata and does not need to be specified in configuration",
