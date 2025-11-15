0680b7ca9bbf5997e66bec7225e89afbfd6a9e7e
Paul Haesler <linkjuggler@gmail.com>
2025-11-15 16:40:46 +1100

    How did this work before??

--- datacube_ows/sql/postgres/ows_schema/bootstrap/001_postgis_extension_isolated.sql
+++ datacube_ows/sql/postgres/ows_schema/bootstrap/001_postgis_extension_isolated.sql
@@ -2,4 +2,4 @@
 
 -- Note "bootstrap" scripts need to be run by a database superuser
 
-create extension if not exists postgis
+create extension if not exists POSTGIS
--- docker/files/create-pg-databases.sh
+++ docker/files/create-pg-databases.sh
@@ -5,4 +5,7 @@ for db in "odc_postgres" "odc_postgis"; do
     CREATE DATABASE $db;
     GRANT ALL PRIVILEGES ON DATABASE $db TO opendatacubeusername;
 EOF
+  psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" db <<-EOF
+    CREATE EXTENSION POSTGIS;
+EOF
 done
