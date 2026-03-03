opendatacube/datacube-ows

commit ba3e0bb5ef9cafadd5aa7b0801b2933946f03790

loading: raise exception on failure
Some upper layer seem to be expecting
a WMSException, so catch any exceptions
from loading data and raise a WMSException
from the exceptions from loading data.

Fixes #1449

datacube_ows/loading.py
@@ -311,22 +311,26 @@ class DataStacker:
                 continue
             measurements = pbq.products[0].lookup_measurements(pbq.bands)
             fuse_func = pbq.fuse_func
-            if pbq.manual_merge and len(datasets) > 0:
-                qry_result = self.manual_data_stack(
-                    datasets,
-                    measurements,
-                    pbq.bands,
-                    skip_corrections,
-                    fuse_func=fuse_func,
+            try:
+                qry_result = (
+                    self.manual_data_stack(
+                        datasets,
+                        measurements,
+                        pbq.bands,
+                        skip_corrections,
+                        fuse_func=fuse_func,
+                    )
+                    if pbq.manual_merge and len(datasets) > 0
+                    else self.read_data(
+                        datasets,
+                        measurements,
+                        self._geobox,
+                        resampling=self._resampling,
+                        fuse_func=fuse_func,
+                    )
                 )
-            else:
-                qry_result = self.read_data(
-                    datasets,
-                    measurements,
-                    self._geobox,
-                    resampling=self._resampling,
-                    fuse_func=fuse_func,
-                )
+            except Exception as e:
+                raise WMSException("Error loading data") from e
             if qry_result is None:
                 continue
             if data is None:

commit cf0dfde55eff71c605494935ee5e0a727193dc96

loading: catch exception higher up
Commit ba3e0bb introduced a try-catch
higher up in the call stack, so remove
the lower level and log at the upper
level instead.

datacube_ows/loading.py
@@ -330,6 +330,9 @@ class DataStacker:
                     )
                 )
             except Exception as e:
+                _LOG.error(
+                    "Error (%s) in DataStacker.data(): %s", e.__class__.__name__, str(e)
+                )
                 raise WMSException("Error loading data") from e
             if qry_result is None:
                 continue
@@ -444,20 +447,16 @@ class DataStacker:
         fuse_func: FuserFunc | None = None,
     ) -> xarray.Dataset:
         CredentialManager.check_cred()
-        try:
-            return datacube.Datacube.load_data(
-                datasets,
-                geobox,
-                measurements=measurements,
-                fuse_func=fuse_func,
-                skip_broken_datasets=skip_broken,
-                patch_url=self._layer.patch_url,
-                resampling=resampling,
-                driver=self.cfg.load_driver,
-            )
-        except Exception as e:
-            _LOG.error("Error (%s) in load_data: %s", e.__class__.__name__, str(e))
-            raise
+        return datacube.Datacube.load_data(
+            datasets,
+            geobox,
+            measurements=measurements,
+            fuse_func=fuse_func,
+            skip_broken_datasets=skip_broken,
+            patch_url=self._layer.patch_url,
+            resampling=resampling,
+            driver=self.cfg.load_driver,
+        )
 
     # TODO: Make skip_broken passed in via config
     @log_call