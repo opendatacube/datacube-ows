b5d2cbc6322eb8bf47e136bba4a3a5823aa50b22
Paul Haesler <linkjuggler@gmail.com>
2026-04-07 15:10:11 +1000

    Serialise access to OWSConfig singleton initialisation. (#1534)
    * Serialise access to OWSConfig singleton initialisation.
    
    * Avoid unnecessary function calls in lock.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- datacube_ows/ows_configuration.py
+++ datacube_ows/ows_configuration.py
@@ -20,6 +20,7 @@ import math
 import os
 from collections.abc import Iterable, Mapping
 from enum import Enum
+from threading import Lock
 from typing import Any, Optional, Union, cast
 
 import numpy
@@ -1975,20 +1976,32 @@ class OWSConfig(OWSMetadataConfig):
         return hdrs
 
 
+config_init_lock = Lock()
+
+
 def get_config(
     refresh: bool = False,
     called_from_update_ranges: bool = False,
     make_ready: bool = True,
 ) -> OWSConfig:
-    cfg = OWSConfig(
-        refresh=refresh, called_from_update_ranges=called_from_update_ranges
-    )
-    if make_ready and not cfg.ready:
-        try:
-            with contextlib.suppress(ODCInitException):
-                cfg.make_ready()
-        except OperationalError as e:
-            raise ConfigException(
-                f"Database outage while reading configuration: {e}."
-            ) from None
+    with config_init_lock:
+        if (
+            OWSConfig._instance
+            and OWSConfig.initialised
+            and not refresh
+            and not called_from_update_ranges
+        ):
+            cfg = OWSConfig._instance
+        else:
+            cfg = OWSConfig(
+                refresh=refresh, called_from_update_ranges=called_from_update_ranges
+            )
+        if make_ready and not cfg.ready:
+            try:
+                with contextlib.suppress(ODCInitException):
+                    cfg.make_ready()
+            except OperationalError as e:
+                raise ConfigException(
+                    f"Database outage while reading configuration: {e}."
+                ) from None
     return cfg
