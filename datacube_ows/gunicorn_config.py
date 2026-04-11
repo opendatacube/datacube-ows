213acd2b54d49b667f7274e896586a0bd6c6b12e
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2026-04-11 16:15:01 +0200

    gunicorn_config: import prometheus locally

--- datacube_ows/gunicorn_config.py
+++ datacube_ows/gunicorn_config.py
@@ -8,9 +8,11 @@
 
 import os
 
-from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
-
 
 def child_exit(server, worker) -> None:
     if os.environ.get("PROMETHEUS_MULTIPROC_DIR", False):
+        from prometheus_flask_exporter.multiprocess import (
+            GunicornInternalPrometheusMetrics,
+        )
+
         GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
